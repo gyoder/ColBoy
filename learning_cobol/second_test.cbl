5d5f1a31e6ce20a087ce7480f821d661e8de44de
gyoder <70408179+gyoder@users.noreply.github.com>
2025-03-09 15:44:53 -0400

    more learning

--- learning_cobol/second_test.cbl
+++ learning_cobol/second_test.cbl
@@ -2,7 +2,15 @@
        program-id. sec.
        environment division.
        data division.
+       working-storage section.
+           01 DEF-X pic X value 'a'.
+           01 DEF-NUM pic 9(3) comp value 255.
        procedure division.
            display "Second program".
+           compute DEF-NUM = DEF-NUM + 1.
+           display "NUM: ", DEF-NUM.
+
+           display "X: ", function numval(DEF-X).
+           call "hello_world".
       *    stop run.
        end program sec.
